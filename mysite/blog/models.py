f3c0490f3ea778ddf6dea6205b67479a15f53703
Gurmeet Singh <gbhatia266@gmail.com>
2024-01-27 02:57:03 +0530

    add the tag functionality to project

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
      def get_queryset(self):
@@ -24,6 +25,7 @@ class Post(models.Model):
                              default=Status.DRAFT)
     objects=models.Manager()
     published=PublishedManager()
+    tags=TaggableManager()
     class Meta:
         ordering=['-publish']
         indexes=[
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,15 +5,35 @@ from .forms import EmailPostForm,CommentForm
 from django.core.mail import send_mail
 from django.conf import settings
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 # Create your views here.
-# def post_list(request):
-#     posts=Post.published.all()
-#     return render(request,'blog/post/list.html',{'posts':posts})
-class PostListView(ListView):
-    queryset=Post.published.all()
-    context_object_name='posts'
-    paginate_by=3
-    template_name='blog/post/list.html'
+def post_list(request, tag_slug=None):
+ post_list = Post.published.all()
+ tag = None
+ if tag_slug:
+    tag = get_object_or_404(Tag, slug=tag_slug)
+    post_list = post_list.filter(tags__in=[tag])
+ # Pagination with 3 posts per page
+ paginator = Paginator(post_list, 3)
+ page_number = request.GET.get('page', 1)
+ try:
+    posts = paginator.page(page_number)
+ except PageNotAnInteger:
+ # If page_number is not an integer deliver the first page
+    posts = paginator.page(1)
+ except EmptyPage:
+ # If page_number is out of range deliver last page of results
+    posts = paginator.page(paginator.num_pages)
+ return render(request,
+ 'blog/post/list.html',
+ {'posts': posts,
+ 'tag': tag})
+# class PostListView(ListView):
+#     queryset=Post.published.all()
+#     context_object_name='posts'
+#     paginate_by=3
+#     template_name='blog/post/list.html'
 
 def post_detail(request,year,month,day,post):
     post=get_object_or_404(Post,
