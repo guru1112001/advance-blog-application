795ab38aa9e791363c5087d177c07e3ea62998b6
Gurmeet Singh <gbhatia266@gmail.com>
2024-01-26 13:55:45 +0530

    created the functionality to comment on post

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,8 +1,14 @@
 from django import forms
+from .models import Comments
 
-class EmailPostEmail(forms.Form):
+class EmailPostForm(forms.Form):
     name=forms.CharField(max_length=25)
     email=forms.EmailField()
     to=forms.EmailField()
     comments=forms.CharField(required=False,
                              widget=forms.Textarea)
+
+class CommentForm(forms.ModelForm):
+ class Meta:
+    model = Comments
+    fields = ['name', 'email', 'body']
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render,get_object_or_404
-from .models import Post
+from .models import Post,Comments
 from django.views.generic import ListView
-from .forms import EmailPostEmail
+from .forms import EmailPostForm,CommentForm
 from django.core.mail import send_mail
 from django.conf import settings
+from django.views.decorators.http import require_POST
 # Create your views here.
 # def post_list(request):
 #     posts=Post.published.all()
@@ -21,13 +22,17 @@ def post_detail(request,year,month,day,post):
                            publish__year=year,
                            publish__month=month,
                            publish__day=day)
-    return render(request,'blog/post/detail.html',{'post':post})
+    comments=post.comments.filter(active=True)
+    form=CommentForm()
+    return render(request,'blog/post/detail.html',{'post':post,
+                                                   'comments':comments,
+                                                   'form':form})
 
 def post_share(request,post_id):
     post=get_object_or_404(Post,id=post_id,status=Post.Status.PUBLISHED)
     sent=False
     if request.method=='POST':
-        form=EmailPostEmail(request.POST)
+        form=EmailPostForm(request.POST)
         if form.is_valid():
             cd=form.cleaned_data
             post_url = request.build_absolute_uri(
@@ -40,14 +45,22 @@ def post_share(request,post_id):
             [cd['to']])
             sent = True
     else:
-        form=EmailPostEmail()
+        form=EmailPostForm()
     return render(request,'blog/post/share.html',{'post':post,'form':form,'sent': sent})
-
-
-
-
-
-
+@require_POST
+def post_comment(request,post_id):
+    post=get_object_or_404(Post,id=post_id,status=Post.Status.PUBLISHED)
+    comment=None
+    form=CommentForm(data=request.POST)
+    if form.is_valid():
+        comment=form.save(commit=False)
+        comment.post=post
+        print(comment.post)
+        comment.save()
+    return render(request,'blog/post/comment.html',{
+        'post':post,
+        'form':form,
+        'comment':comment})
 
 
 
