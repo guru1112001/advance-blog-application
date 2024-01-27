f18644e4b742d8dedd0a4558b038bbd660027051
Gurmeet Singh <gbhatia266@gmail.com>
2024-01-28 01:59:19 +0530

    add functionality to recommand similar post by tags

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post,Comments
-from django.views.generic import ListView
+# from django.views.generic import ListView
+from django.db.models import Count
 from .forms import EmailPostForm,CommentForm
 from django.core.mail import send_mail
 from django.conf import settings
@@ -42,11 +43,17 @@ def post_detail(request,year,month,day,post):
                            publish__year=year,
                            publish__month=month,
                            publish__day=day)
+    
     comments=post.comments.filter(active=True)
     form=CommentForm()
+    post_tags_ids=post.tags.values_list("id",flat=True)
+    similar_posts=Post.published.filter(tags__in=post_tags_ids,)\
+                                        .exclude(id=post.id)
+    similar_posts=similar_posts.annotate(same_tags=Count("tags"))\
+                                            .order_by('-same_tags','-publish')[:4]
     return render(request,'blog/post/detail.html',{'post':post,
                                                    'comments':comments,
-                                                   'form':form})
+                                                   'similar_posts':similar_posts})
 
 def post_share(request,post_id):
     post=get_object_or_404(Post,id=post_id,status=Post.Status.PUBLISHED)
